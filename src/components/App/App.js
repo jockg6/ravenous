7f5581523c1d6646633eb50ff8afde56d76a36ab
jockg6 <allister.grady@mipac.com.au>
2020-07-21 21:34:05 +1000

    complete ravenous part 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,11 +25,19 @@ const businesses = [
 ];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.searchYelp = this.searchYelp.bind(this);
+  }
+  searchYelp(term, location, sortBy) {
+    alert('Searching Yelp with '+ term + ', ' + location + ', ' + sortBy);
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
-        <SearchBar />
+        <h1>ravenous ??</h1>
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
